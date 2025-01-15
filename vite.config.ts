2bedf34f52e7fbbc7ed9f8d41f09d028e27b3d3a
richxch3n <100868143+richxch3n@users.noreply.github.com>
2025-01-15 14:17:18 +0800

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Set this to the correct subdirectory
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
